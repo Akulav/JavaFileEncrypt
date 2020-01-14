5dd6b65c6b7f7d7c534d293b2bed5da25c22e82d
Akulav <43496076+Akulav@users.noreply.github.com>
2020-01-15 00:22:06 +0200

    Rewritten file wrtie/read
    -Now can read files of any sizes (in chunks)

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 package application;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Random;
+
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -27,28 +33,20 @@ public class Main extends Application {
 	private Button decrypt;
 	@FXML
 	private PasswordField key;
-
-	static Storage s = new Storage();
+	@FXML
+	private Label SelectedPath;
 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			
 			Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
 			Scene scene = new Scene(root);
 			primaryStage.getIcons().add(new Image("triangle-512.png"));
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Made By Catalin");
 			primaryStage.setResizable(false);
-			FileChooser fileChooser = new FileChooser();
-			File file = fileChooser.showOpenDialog(primaryStage);
-
 			primaryStage.show();
-			while (file == null) {
-				file = fileChooser.showOpenDialog(primaryStage);
-				path.setText(file.toString());
-			}
-			s.set(file);
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -58,46 +56,110 @@ public class Main extends Application {
 		launch(args);
 	}
 
-	public void selectEncrypt() throws IOException {
-		byte[] fileBytes = Files.readAllBytes(s.get().toPath());
-		int cipher = Integer.valueOf(key.getText());
-		for (int i = 0; i < fileBytes.length; i++) {
-			fileBytes[i] = (byte) (fileBytes[i] + cipher);
+	static Storage s = new Storage();
 
+	public void getPath() {
+		Stage primaryStage = new Stage();
+		FileChooser fileChooser = new FileChooser();
+		File file = fileChooser.showOpenDialog(primaryStage);
+		while (file == null) {
+			file = fileChooser.showOpenDialog(primaryStage);
+			path.setText(file.toString());
 		}
-		System.out.println("Applied Cipher");
-		try (FileOutputStream fos = new FileOutputStream(s.get())) {
-			fos.write(fileBytes);
-			System.out.println("Successfully written data to the file");
-		} catch (IOException e) {
-			e.printStackTrace();
+		s.set(file);
+		SelectedPath.setText(file.toString());
+	}
+
+	public void selectEncrypt() throws IOException {
+
+		try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(s.get()))) {
+			byte[] bbuf = new byte[1048576];
+			byte[] wow = new byte[1048576];
+			int len;
+			String fileString = convertPath(s.get().toString());
+			fileString = fileString + "\\" + genName();
+			System.out.println(fileString);
+			File file = new File(fileString);
+			int cipher = Integer.valueOf(key.getText());
+			while ((len = in.read(bbuf)) != -1) {
+				for (int i = 0; i < bbuf.length; i++) {
+					bbuf[i] = (byte) (bbuf[i] + cipher);
+				}
+				try (FileOutputStream fos = new FileOutputStream(file, true)) {
+					fos.write(wow);
+					fos.write(bbuf);
+					System.out.println("Success");
+					progress.setText("Success");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			in.close();
+			Files.deleteIfExists(s.get().toPath());
 		}
+
+	}
+
+	public String convertPath(String s) {
+		int index = s.lastIndexOf('\\');
+		String converted = s.substring(0, index);
+		return converted;
+	}
+	
+	public int genName() {
+		Random rand = new Random();
+		int rand_name1 = rand.nextInt(10000);
+		int rand_name2 = rand.nextInt(10000);
+		int rand_name3 = rand.nextInt(10000);
+		int finalName = rand_name1+rand_name2+rand_name3;
+		return finalName;
+	}
+	
+	public int genDecryptFlag() {
+		Random rand = new Random();
+		int flag = rand.nextInt(10);
+		while (flag%2!=0 && flag > 999) {
+			flag=rand.nextInt(99999);
+		} 
+		
+		int flag2 = rand.nextInt(10);
+		while (flag2%2!=0 && flag2 > 999) {
+			flag2=rand.nextInt(99999);
+		}
+		
+		int flag3 = rand.nextInt(10);
+		while (flag3%2!=0 && flag3 > 999) {
+			flag3=rand.nextInt(99999);
+		} 
+		
+		System.out.println(flag);
+		return flag;
 	}
 
 	public void selectDecrypt() throws IOException {
-		byte[] fileBytes = Files.readAllBytes(s.get().toPath());
-		int cipher = Integer.valueOf(key.getText());
-		for (int i = 0; i < fileBytes.length; i++) {
-			fileBytes[i] = (byte) (fileBytes[i] - cipher);
-		}
-		System.out.println("Applied Cipher");
-		try (FileOutputStream fos = new FileOutputStream(s.get())) {
-			fos.write(fileBytes);
-			System.out.println("Successfully written data to the file");
-		} catch (IOException e) {
-			e.printStackTrace();
+
+		try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(s.get()))) {
+			byte[] bbuf = new byte[1048576];
+			int len;
+			String fileString = convertPath(s.get().toString());
+			fileString = fileString + "\\" + genName();
+			System.out.println(fileString);
+			File file = new File(fileString);
+			int cipher = Integer.valueOf(key.getText());
+			while ((len = in.read(bbuf)) != -1) {
+				for (int i = 0; i < bbuf.length; i++) {
+					bbuf[i] = (byte) (bbuf[i] - cipher);
+				}
+				try (FileOutputStream fos = new FileOutputStream(file, true)) {
+					fos.write(bbuf);
+					System.out.println("Success");
+					progress.setText("Success");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			in.close();
+			Files.deleteIfExists(s.get().toPath());
 		}
 	}
 }
-
-/*
- * File file = fileChooser.showOpenDialog(primaryStage); while (file == null) {
- * file = fileChooser.showOpenDialog(primaryStage); } try (Writer writer = new
- * BufferedWriter(new OutputStreamWriter( new FileOutputStream("E:/path"),
- * "utf-8"))) { writer.write(file.toString()); writer.close(); }
- */
-/*
- * final DirectoryChooser directoryChooser = new DirectoryChooser(); final File
- * selectedDirectory = directoryChooser.showDialog(stage); if (selectedDirectory
- * != null) { selectedDirectory.getAbsolutePath(); }
- */
